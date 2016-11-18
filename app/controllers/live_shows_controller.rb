7660fb008bb2b2cb80ccb7d1e62361e920066737
PCboyLin <pcboylinmail@gmail.com>
2016-11-18 16:08:10 +0800

    - layout,show

--- app/controllers/live_shows_controller.rb
+++ app/controllers/live_shows_controller.rb
@@ -1,11 +1,11 @@
 class LiveShowsController < ApplicationController
-  def index
-    @live_shows = LiveShow.includes(:user)
-  end
+ def index
+   @live_shows = LiveShow.includes(:user)
+ end
 
-  def show
-    @user = User.find(params[:user_id])
-    @live_show = @user.live_shows.find(params[:id])
-    @live_shows = @user.live_shows
-  end
+ def show
+   # @live_show = LiveShow.find(params[:id])
+   # @user = @live_show.user
+   # @live_shows = @user.live_shows
+ end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   #devise_for :users
-  root :to => "homes#index"
+  root :to => "live_shows#show"
   resources :homes do 
     collection do
 
@@ -12,19 +12,19 @@ Rails.application.routes.draw do
   namespace :api, :defaults => { :format => :json } do
     post "login" => "auth#login"
     post "logout" => "auth#logout"
-    resources :users
-    resources :live_shows do
-      resources :followings
-      resources :chats
-      resources :questions do
-        resources :askings
-      end
-    end
+    
+resources :users
+ resources :live_shows do
+   resources :followings
+   resources :chats
+   resources :questions do
+     resources :askings
+   end
+ end
   end
   resources :users do
     resources :live_shows
   end
 
-  get '/g_index' => 'homes#g_index' , :as => 'g_index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
